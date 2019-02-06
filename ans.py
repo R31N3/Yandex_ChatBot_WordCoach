ef3d057b1c1e21df47cb84f3fed0eaac8d96f787
atryapa <atryapa@gmail.com>
2019-02-06 20:16:16 +0300

    +easy db access function

--- ans.py
+++ ans.py
@@ -8,18 +8,31 @@ def classify(text, mode):
         text = text.strip()
     else:
         add_word = False
+    if text[0] == '-':
+        del_word = True
+        text = text[1:]
+        text = text.strip()
+    else:
+        del_word = False
     words = text.split()
     if (words[0].startswith('алиса') or words[0].startswith('alice')) and len(words) > 1:
         words = words[1:]
     if words[0].startswith('add') or words[0].startswith('добавь'):
         add_word = True
         words = words[1:]
+    if words[0].startswith('delete') or words[0].startswith('del') or words[0].startswith('удали'):
+        del_word = True
+        words = words[1:]
     if add_word or mode == 0:
         if mode != 0:
             warning = True
         else:
             warning = False
-        if len(words) == 1:
+        if del_word:
+            answer = ''
+            label = 'incorrect'
+            warning = True
+        elif len(words) == 1:
             label = 'suggest_some_translation&suggest_to_add'
             answer = words[:]
         elif len(words) == 2:
@@ -28,6 +41,13 @@ def classify(text, mode):
         else:
             label = 'translate&suggest_to_add'
             answer = ' '.join(words)
+    elif del_word:
+        if mode != 0:
+            warning = True
+        else:
+            warning = False
+        label = 'del'
+        answer = ' '.join(words)
     else:
         label = 'use_mode'
         answer = text
