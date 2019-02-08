220394d75247c07ae6a0bfe313cc6a0f78039978
atryapa <atryapa@gmail.com>
2019-02-08 12:24:43 +0300

    +look little functions provide database operations

--- ans.py
+++ ans.py
@@ -67,6 +67,3 @@ print(classify('Алиса, добавь музло', 'game'))
 print(classify('Алиса, добавь музло дважды трижды', 'game'))
 print(classify('-a', 3))
 print(classify('-a', 0))
-a = {'a' : 'b', 'c' : 'd'}
-a['c'] = None
-print(a)
