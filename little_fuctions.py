65159dee153a16c4414c5ef0f449dc2420ac5f3a
atryapa <atryapa@gmail.com>
2019-02-08 19:43:15 +0300

    +look little functions provide database operations

--- little_fuctions.py
+++ little_fuctions.py
@@ -161,3 +161,4 @@ def update_dictionary(id, words_to_add, database):
     database.update_entries('users_info', id, {'learned_eng_words': "#$".join(learned_eng_words),
                                                'learned_rus_words': "#$".join(learned_rus_words)}, update_type='rewrite')
 
+print()
