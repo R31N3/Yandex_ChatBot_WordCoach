7ed4faa6d3a64b4ed8b69d18c7c46adddc627e55
atryapa <atryapa@gmail.com>
2019-02-15 00:17:59 +0300

    SUCSEX

--- main_function.py
+++ main_function.py
@@ -27,10 +27,11 @@ def message_return(response, user_storage, message, button, database, request, m
     else:
         response.set_text(message.replace('+;', '##!').replace('+', '').replace('##!', '+').replace(' pause ', ' '))
 
-    message = message.replace('\n', ' - - ').replace(' pause ', ' - ') + ' - - '
+    message = message.replace('\n', ' - - ').replace(' pause ', ' - ') + ' - '
     message = message.replace('(c)', ' - ').replace('(с)', ' - ')
     message = message.replace('(Английская пословица)', ' - Английская пословица.')
     message = message.replace('Гранде', 'Грандэ')
+    message.replace('4000 строк', 'четырех тысяч строк')
     button, something = get_suggests({'suggests' : list(map(lambda x:\
         x.replace(' pause ', ' ').replace('+ ', '##!').replace('+', '').replace('##!', '+'), user_storage['suggests']))})
     if mode != 'training' and mode != 'settings' and not mode.startswith('add_set') and \
