6c70c50009abc9a2ca0426b86a106d5dd6f7658c
R31N3 <dfinogeev3@yandex.ru>
2019-02-21 21:51:37 +0300

    test

--- little_fuctions.py
+++ little_fuctions.py
@@ -236,10 +236,7 @@ def get_suggests(user_storage):
             if type(suggest) != list:
                 suggests.append({'title': suggest, 'hide': True})
             else:
-                print(suggest)
-                suggests.append({'title': suggest[0],
-                                 "payload": {}, "url": suggest[1], 'hide': True})
-                print(suggests)
+                suggests.append({'title': suggest[0], "url": suggest[1], 'hide': False})
     else:
         suggests = []
 
