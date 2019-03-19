12050c2dada5458822d0598c617bd25037f8f77d
R31N3 <dfinogeev3@yandex.ru>
2019-03-19 17:51:02 +0300

    fix

--- main_function.py
+++ main_function.py
@@ -851,7 +851,7 @@ def handle_dialog(request, response, user_storage, database, morph):
 
     elif handle == 'use_mode':
         if get_mode(user_id, database) == 'training':
-            output_message = training.main(get_q(user_id, database), answer, 'revise&next', user_id, database)
+            output_message = training.main(get_q(user_id, database), answer, 'revise&next', user_id, database, request)
             if get_mode(user_id, database) == 'training':
                 but = training.get_buttons(get_q(user_id, database), user_id, database)
                 stor = {'suggests': but + (['Закончить тренировку'] if get_q(user_id, database) != '###empty' else [])}
